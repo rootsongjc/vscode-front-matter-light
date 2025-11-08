54c05b2e47391a25c1779c42511ac6ddb8824517
Jimmy Song <rootsongjc@gmail.com>
2025-11-08 23:33:47 +0800

    Update icon/logo

--- src/constants/Extension.ts
+++ src/constants/Extension.ts
@@ -1,7 +1,7 @@
 export const EXTENSION_COMMAND_PREFIX = 'frontMatter';
 
-export const EXTENSION_ID = 'jimmmysong.hugo-cms';
-export const EXTENSION_BETA_ID = 'jimmmysong.hugo-cms-beta';
+export const EXTENSION_ID = 'jimmysong.hugo-cms';
+export const EXTENSION_BETA_ID = 'jimmysong.hugo-cms-beta';
 
 export const getCommandName = (command: string) => {
   return `${EXTENSION_COMMAND_PREFIX}.${command}`;
